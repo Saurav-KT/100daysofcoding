b5c71ed747610c91273af9744959749a3b53c122
saurav <Saurav_Kumar_jha@Dellteam.com>
2022-11-29 20:02:11 +0530

    Return indices of the two numbers such that they add up to target.

---
+++ find_array_index/find_array_index.py
@@ -0,0 +1,30 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+# Example 2:
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+#
+# Example 3: Input: nums = [3,3], target = 6
+# Output: [0,1]
+
+def findarrindex(num, target):
+    result = 0
+    return_index = []
+    for element in range(len(num)):
+        for inner_element in range(element + 1, len(num)):
+            result = num[element] + num[inner_element]
+            if result == target:
+                return_index.append([element, inner_element])
+                # return_index = [element, inner_element]
+                # return return_index
+    return return_index
+
+
+nums = [3, 2, 4, 1, 5, 3]
+target = 6
+print(findarrindex(nums, target))
