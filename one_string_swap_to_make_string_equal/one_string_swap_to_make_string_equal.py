09f79969b92810edbdc0f0ab34f59d8d61206f6b
saurav <Saurav_Kumar_jha@Dellteam.com>
2022-12-27 11:05:21 +0530

    Check if One String Swap Can Make Strings Equal

---
+++ one_string_swap_to_make_string_equal/one_string_swap_to_make_string_equal.py
@@ -0,0 +1,15 @@
+# Leetcode problem: Check if One String Swap Can Make Strings Equal
+
+def areAlmostEqual(self, s1: str, s2: str) -> bool:
+    mismatch = []
+    for i, j in zip(s1, s2):
+        if i != j:
+            mismatch.append((i, j))
+
+    if len(mismatch) == 0:
+        return True
+    if len(mismatch) != 2:
+        return False
+    a1, b1 = mismatch[0]
+    a2, b2 = mismatch[1]
+    return a1 == b2 and b1 == a2
