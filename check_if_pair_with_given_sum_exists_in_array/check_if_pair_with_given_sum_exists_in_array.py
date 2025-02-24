e24e4a273b2fa7e66d662a7f41730e03f91278e2
saurav j <saurav.saurav12@gmail.com>
2025-02-24 17:47:52 +0530

    find a pair in an array that sums up to a given target

---
+++ check_if_pair_with_given_sum_exists_in_array/check_if_pair_with_given_sum_exists_in_array.py
@@ -0,0 +1,43 @@
+# Problem: find a pair in an array that sums up to a given target
+# scenario to test
+# 1. Input array can't be empty
+# 2. Pair doesnot exist in array
+# 3. single element in input array can't form a pair
+
+
+class FindPair:
+
+  # approach-1: using a brute-force approach
+  @staticmethod
+  def find_pair(arr: list,n:int):
+      if not arr or len(arr)==1:
+          raise ValueError("Input array can't be empty or single element in input array can't form a pair")
+      for i in range(len(arr)-1):
+          for j in range(i+1, len(arr)-1):
+              if (arr[i]+arr[j])==n:
+                  return arr[i],arr[j]
+      raise ValueError("No pair found")
+
+
+
+
+   # approach-2: using a hash set for optimal performance O(n) time complexity
+
+
+    # @staticmethod
+    # def find_pair(arr: list, n: int):
+    #     if not arr:
+    #         raise ValueError("Input array can't be empty")
+    #     seen = []
+    #     for num in arr:
+    #         complement = n - num
+    #         if complement in seen:
+    #             return complement,num
+    #         seen.append(num)
+    #     return False
+
+
+input_arr=[3,5,8,2,16]
+target=10
+print((FindPair.find_pair(input_arr,target)))
+
