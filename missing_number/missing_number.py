97f30da3b3d04a38120afba928b424f227ebc4a6
saurav j <saurav.saurav12@gmail.com>
2025-03-03 20:27:55 +0530

    description change

--- missing_number/missing_number.py
+++ missing_number/missing_number.py
@@ -1,4 +1,5 @@
-# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+# Given an array nums containing n distinct numbers in the range [0, n],
+# return the only number in the range that is missing from the array.
 # Input: nums = [3,0,1]
 # Output: 2
 # Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
@@ -21,15 +22,6 @@
 # num = [0, 2, 3]
 # print(missingNumber(num))
 
-
-# def creating_gen(index):
-#     months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-#     yield months[index]
-#     yield months[index + 2]
-#
-#
-# next_month = creating_gen(3)
-# print(next(next_month), next(next_month))
 
 
 
